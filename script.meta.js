8fbadd67fc64afd6fff309340b5045ee97f7a961
mazm <marcel.zipaj-misko@student.tuke.sk>
2025-04-13 16:02:31 +0200

    initial commit

---
+++ script.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Jánošík
+// @namespace    https://macko.industries.sk/dusan
+// @version      1.0.0
+// @description  Bohatým bral, bohatším dával.
+// @author       ME or HE
+// @match        https://sk9.the-west.sk/game.php
+// @icon         https://westsk.innogamescdn.com/images/items/left_arm/bear.png?5
+// @grant        none
+// ==/UserScript==
